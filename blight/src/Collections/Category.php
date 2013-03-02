2b8aacd668efe5508520c065c63d6ba719c8293d
Adam Averay <adam.averay@gmail.com>
2013-03-02 10:45:36 -0500

    Fixed incorrect class name for Category

--- blight/src/Collections/Category.php
+++ blight/src/Collections/Category.php
@@ -1,7 +1,7 @@
 <?php
 namespace Blight\Collections;
 
-class Tag extends Collection implements \Blight\Interfaces\Collection {
+class Category extends Collection implements \Blight\Interfaces\Collection {
 	public function get_url(){
 		return $this->blog->get_url('category/'.$this->get_slug());
 	}
