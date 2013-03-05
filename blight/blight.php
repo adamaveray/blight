74ab306b42af6a3b687fe194dad850b6afc01dec
Adam Averay <adam.averay@gmail.com>
2013-03-05 11:28:02 -0500

    Moved redirection headers to web index file

--- blight/blight.php
+++ blight/blight.php
@@ -63,7 +63,3 @@ $renderer	= new Renderer($blog, $manager);
 	));
 
 // Rendering completed
-
-// Redirect to generated pages
-header('HTTP/1.1 302 Found');
-header('Location: '.$_SERVER['REQUEST_URI']);
--- www/index.php
+++ www/index.php
@@ -1,3 +1,7 @@
 <?php
 $web_path	= __DIR__;
 require('../blight/blight.php');
+
+// Redirect to generated pages
+header('HTTP/1.1 302 Found');
+header('Location: '.$_SERVER['REQUEST_URI']);
