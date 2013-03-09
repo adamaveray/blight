2c57e8d8d53be310d0d9cc7d4b2c76350a67f703
Adam Averay <adam.averay@gmail.com>
2013-03-09 18:55:11 -0500

    Created additional tests for Renderer class

--- tests/Blight/RendererTest.php
+++ tests/Blight/RendererTest.php
@@ -16,10 +16,13 @@ class RendererTest extends \PHPUnit_Framework_TestCase {
 	}
 
 	static protected function deleteDir($dir){
-		$files	= glob(rtrim($dir, '/').'/*');
 		if(!is_dir($dir)){
 			return;
 		}
+
+		$dir	= rtrim($dir, '/');
+
+		$files	= glob($dir.'/'.'*');
 
 		foreach($files as $file){
 			if(is_dir($file)){
@@ -36,13 +39,14 @@ class RendererTest extends \PHPUnit_Framework_TestCase {
 	protected $blog;
 	/** @var \Blight\Interfaces\Manager */
 	protected $manager;
-	/** @var \Blight\Interfaces\Renderer */
+	/** @var \Blight\Renderer */
 	protected $renderer;
 
 	public function setUp(){
 		global $config;
 		$test_config	= $config;
 		$test_config['paths']['web']		= __DIR__.'/files/web/';
+		$test_config['paths']['drafts-web']	= __DIR__.'/files/web/_drafts/';
 		$test_config['paths']['templates']	= __DIR__.'/files/templates/';
 		$this->blog		= new \Blight\Blog($test_config);
 
@@ -115,16 +119,70 @@ EOD;
 	/**
 	 * @covers \Blight\Renderer::render_drafts
 	 */
-	/*public function testRenderDrafts(){
+	public function testRenderDrafts(){
+		$content	= <<<EOD
+Test Post
+=========
+Date: 2013-02-01
+
+Test content
+EOD;
+
+		$posts	= array(
+			new \Blight\Post($this->blog, $content, 'test-1', true),
+			new \Blight\Post($this->blog, $content, 'test-2', true),
+			new \Blight\Post($this->blog, $content, 'test-3', true)
+		);
+		$this->manager->set_mock_posts($posts, 'drafts');
+
 		$this->renderer->render_drafts();
-	}*/
+
+		$output_dir	= $this->blog->get_path_drafts_web();
+		$files	= glob($output_dir.'*');
+
+		$this->assertEquals(count($files), count($posts));
+
+		foreach($posts as $post){
+			// Check each post had file created with same slug
+			$this->assertTrue(in_array($output_dir.$post->get_slug().'.html', $files));
+		}
+	}
 
 	/**
 	 * @covers \Blight\Renderer::render_archives
 	 */
-	/*public function testRenderArchives(){
-		$this->renderer->render_drafts();
-	}*/
+	public function testRenderArchives(){
+		$content	= <<<EOD
+Test Post
+=========
+Date: 2013-02-01
+
+Test content
+EOD;
+
+		$posts	= array(
+			new \Blight\Post($this->blog, $content, 'test-1'),
+			new \Blight\Post($this->blog, $content, 'test-2'),
+			new \Blight\Post($this->blog, $content, 'test-3')
+		);
+
+		$date_counts	= array(
+			'2013'	=> array(
+				'posts'		=> 1,
+				'months'	=> array(
+					'02'	=> 3
+				)
+			)
+		);
+
+		$this->manager->set_mock_posts($posts, 'posts');
+
+		$this->renderer->render_archives();
+
+		$output_dir	= $this->blog->get_path_www().'archive/';
+		$files	= glob($output_dir.'*');
+		$this->assertEquals(count($files), count($date_counts));
+	}
 
 	/**
 	 * @covers \Blight\Renderer::render_year
@@ -136,28 +194,118 @@ EOD;
 	/**
 	 * @covers \Blight\Renderer::render_tags
 	 */
-	/*public function testRenderTags(){
+	public function testRenderTags(){
+		$content	= <<<EOD
+Test Post
+=========
+Date: 2013-02-01
+Tags: Test Tag
+
+Test content
+EOD;
+
+		$posts	= array(
+			new \Blight\Post($this->blog, $content, 'test-1'),
+			new \Blight\Post($this->blog, $content, 'test-2'),
+			new \Blight\Post($this->blog, $content, 'test-3')
+		);
+
+		$this->manager->set_mock_posts($posts, 'posts');
+
+		$tag_counts	= array(
+			'test-tag'	=> 3
+		);
+
+		$dir	= $this->blog->get_path_www('tag/');
+
 		$this->renderer->render_tags();
-	}*/
+
+		foreach($tag_counts as $tag => $count){
+			$this->assertTrue(file_exists($dir.$tag.'.html'));
+		}
+	}
 
 	/**
 	 * @covers \Blight\Renderer::render_categories
 	 */
-	/*public function testRenderCategories(){
+	public function testRenderCategories(){
+		$content	= <<<EOD
+Test Post
+=========
+Date: 2013-02-01
+Category: General
+
+Test content
+EOD;
+
+		$posts	= array(
+			new \Blight\Post($this->blog, $content, 'test-1'),
+			new \Blight\Post($this->blog, $content, 'test-2'),
+			new \Blight\Post($this->blog, $content, 'test-3')
+		);
+
+		$this->manager->set_mock_posts($posts, 'posts');
+
+		$category_counts	= array(
+			'general'	=> 3
+		);
+
+		$dir	= $this->blog->get_path_www('category/');
+
 		$this->renderer->render_categories();
-	}*/
+
+		foreach($category_counts as $category => $count){
+			$this->assertTrue(file_exists($dir.$category.'.html'));
+		}
+	}
 
 	/**
 	 * @covers \Blight\Renderer::render_home
 	 */
-	/*public function testRenderHome(){
+	public function testRenderHome(){
+		$content	= <<<EOD
+Test Post
+=========
+Date: 2013-02-01
+
+Test content
+EOD;
+
+		$posts	= array(
+			new \Blight\Post($this->blog, $content, 'test-1'),
+			new \Blight\Post($this->blog, $content, 'test-2'),
+			new \Blight\Post($this->blog, $content, 'test-3')
+		);
+
 		$this->renderer->render_home();
-	}*/
+
+		$path	= $this->blog->get_path_www('index.html');
+
+		$this->assertTrue(file_exists($path));
+	}
 
 	/**
 	 * @covers \Blight\Renderer::render_feed
 	 */
-	/*public function testRenderFeed(){
+	public function testRenderFeed(){
+		$content	= <<<EOD
+Test Post
+=========
+Date: 2013-02-01
+
+Test content
+EOD;
+
+		$posts	= array(
+			new \Blight\Post($this->blog, $content, 'test-1'),
+			new \Blight\Post($this->blog, $content, 'test-2'),
+			new \Blight\Post($this->blog, $content, 'test-3')
+		);
+
 		$this->renderer->render_feed();
-	}*/
+
+		$path	= $this->blog->get_path_www('feed.xml');
+
+		$this->assertTrue(file_exists($path));
+	}
 };
