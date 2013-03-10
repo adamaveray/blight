015d931c2144fca75e08832f1212d87de5dacaac
Adam Averay <adam.averay@gmail.com>
2013-03-10 15:15:53 -0400

    Created Page class and made parent of Post

---
+++ blight/src/Interfaces/Page.php
@@ -0,0 +1,22 @@
+<?php
+namespace Blight\Interfaces;
+
+interface Page {
+	public function __construct(\Blight\Interfaces\Blog $blog, $content, $slug);
+
+	public function get_title();
+
+	public function get_slug();
+
+	public function get_content();
+
+	public function get_metadata();
+
+	public function get_meta($name);
+
+	public function has_meta($name);
+
+	public function get_permalink();
+
+	public function get_relative_permalink();
+};
