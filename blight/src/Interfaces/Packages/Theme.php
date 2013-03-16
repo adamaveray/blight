e55119a094b0c158038e2f2165dc6892bb6e819a
Adam Averay <adam.averay@gmail.com>
2013-03-16 19:53:44 -0400

    Created initial Theme class

---
+++ blight/src/Interfaces/Packages/Theme.php
@@ -0,0 +1,5 @@
+<?php
+namespace Blight\Interfaces\Packages;
+
+interface Theme extends \Blight\Interfaces\Packages\Package {
+};
---
+++ blight/src/Packages/Theme.php
@@ -0,0 +1,5 @@
+<?php
+namespace Blight\Packages;
+
+abstract class Theme extends Package implements \Blight\Interfaces\Packages\Theme {
+};
