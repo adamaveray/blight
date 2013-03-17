808ba26651cd3538ae4a3ed5c66d5d56dd074c0b
Adam Averay <adam.averay@gmail.com>
2013-03-17 11:25:43 -0400

    Updated File System to allow preservation of file attributes

--- blight/src/FileSystem.php
+++ blight/src/FileSystem.php
@@ -26,21 +26,22 @@ class FileSystem implements \Blight\Interfaces\FileSystem {
 	 *
 	 * @param string $path		The file to be written
 	 * @param string $content	The content to write to the file
+	 * @param bool $match_directory_ownership	Whether to make the file match the ownership attributes of the parent directory
 	 * @throws \RuntimeException	The file cannot be written or the containing directory cannot be made
 	 */
-	public function create_file($path, $content){
+	public function create_file($path, $content, $match_directory_ownership = true){
 		$dir	= pathinfo($path, \PATHINFO_DIRNAME);
 		if(!is_dir($dir)){
-			$result	= mkdir($dir, 0755, true);
-
-			if($result === false){
-				throw new \RuntimeException('Cannot create directory for '.$path);
-			}
+			$this->create_dir($dir);
 		}
 
 		$result	= file_put_contents($path, $content);
 		if($result === false){
 			throw new \RuntimeException('Cannot create '.$path);
+		}
+
+		if($match_directory_ownership){
+			$this->match_file_ownership($dir, $path);
 		}
 	}
 
@@ -72,10 +73,11 @@ class FileSystem implements \Blight\Interfaces\FileSystem {
 	 * @param string $old_path	The current location of the file
 	 * @param string $new_path	The location to move the file to
 	 * @param bool $cleanup		Whether to delete the containing directory if empty
+	 * @param bool $maintain_attributes	Whether to maintain the file's modification date and ownership
 	 * @see delete_file()
 	 */
-	public function move_file($old_path, $new_path, $cleanup = false){
-		$this->copy_file($old_path, $new_path);
+	public function move_file($old_path, $new_path, $cleanup = false, $maintain_attributes = true){
+		$this->copy_file($old_path, $new_path, $maintain_attributes);
 		$this->delete_file($old_path, $cleanup);
 	}
 
@@ -84,10 +86,50 @@ class FileSystem implements \Blight\Interfaces\FileSystem {
 	 *
 	 * @param string $source_path	The path to the original file
 	 * @param string $target_path	The path to the file to create
+	 * @param bool $maintain_attributes	Whether to set the new file's modification date and ownership to match the original
 	 */
-	public function copy_file($source_path, $target_path){
+	public function copy_file($source_path, $target_path, $maintain_attributes = true){
 		$content	= $this->load_file($source_path);
 		$this->create_file($target_path, $content);
+
+		if($maintain_attributes){
+			// Update modification date
+			touch($target_path, filemtime($source_path));
+
+			// Attempt to update owner
+			$this->match_file_ownership($source_path, $target_path);
+		}
+	}
+
+	/**
+	 * Attempts to match the file owner and group of the source file on the target file
+	 *
+	 * @param string $source	The file to copy ownership from
+	 * @param string $target	The file to copy ownership to
+	 * @return bool	Whether the copying was successful
+	 */
+	protected function match_file_ownership($source, $target){
+		try {
+			// Update group
+			if(($source_groupid = filegroup($source)) === false) throw new \Exception('filegroup failed on source');
+			if(($target_groupid = filegroup($target)) === false) throw new \Exception('filegroup failed on target');
+			if($source_groupid != $target_groupid){
+				// Group is different - update
+				if(!@chgrp($target, $source_groupid)) throw new \Exception('chgrp failed on target');
+			}
+
+			// Update user
+			if(($source_userid = fileowner($source)) === false) throw new \Exception('fileowner failed on source');
+			if(($target_userid = fileowner($target)) === false) throw new \Exception('fileowner failed on target');
+			if($source_userid != $target_userid){
+				// User is different - update
+				if(!@chown($target, $source_userid)) throw new \Exception('chown failed on target');
+			}
+		} catch(\Exception $e){
+			return false;
+		}
+
+		return true;
 	}
 
 	/**
@@ -126,16 +168,30 @@ class FileSystem implements \Blight\Interfaces\FileSystem {
 	 * @throws \RuntimeException	Cannot create the directory
 	 * @see mkdir()
 	 */
-	public function create_dir($path, $mode = 0755, $recursive = true){
+	public function create_dir($path, $mode = 0755, $recursive = true, $match_parent_ownership = true){
 		$path	= rtrim($path, '/');
 		if(is_dir($path)){
 			// Already exists
 			return;
 		}
 
-		$result	= mkdir($path, $mode, $recursive);
+		$parent	= pathinfo($path, \PATHINFO_DIRNAME);
+		if(!is_dir($parent)){
+			if(!$recursive){
+				throw new \RuntimeException('Parent directory for '.$path.' does not exist');
+			}
+
+			// Create parent
+			$this->create_dir($parent, $mode, $recursive, $match_parent_ownership);
+		}
+
+		$result	= mkdir($path, $mode);
 		if($result === false){
 			throw new \RuntimeException('Cannot create '.$path);
+		}
+
+		if($match_parent_ownership){
+			$this->match_file_ownership($parent, $path);
 		}
 	}
 
@@ -147,11 +203,13 @@ class FileSystem implements \Blight\Interfaces\FileSystem {
 	 * @param string $target_dir	The directory to copy files to
 	 * @param int $mode				The directory permissions mode to use
 	 * @param bool $recursive		Whether to copy and child directories
+	 * @param bool $maintain_attributes	Whether to set the new directory's modification date and permissions to match the original
+	 * @param bool $check_difference	Whether to overwrite files with the same or newer modification date
 	 *
 	 * @throws \RuntimeException	Source directory does not exist
 	 * @throws \RuntimeException	Cannot create target directory
 	 */
-	public function copy_dir($source_dir, $target_dir, $mode = 0755, $recursive = true){
+	public function copy_dir($source_dir, $target_dir, $mode = 0755, $recursive = true, $maintain_attributes = true, $check_difference = false){
 		$source_dir	= rtrim($source_dir, '/');
 		$target_dir	= rtrim($target_dir, '/');
 
@@ -159,7 +217,10 @@ class FileSystem implements \Blight\Interfaces\FileSystem {
 			throw new \RuntimeException('Source dir does not exist');
 		}
 
-		$this->create_dir($target_dir, $mode, true);
+		$this->create_dir($target_dir, $mode, true, $maintain_attributes);
+		if($maintain_attributes){
+			touch($target_dir, filemtime($source_dir));
+		}
 
 		$source	= new \FilesystemIterator($source_dir);
 		foreach($source as $file){
@@ -170,9 +231,11 @@ class FileSystem implements \Blight\Interfaces\FileSystem {
 					continue;
 				}
 
-				$this->copy_dir($file, $target_file, $mode, $recursive);
-			} else {
-				$this->copy_file($file, $target_file);
+				$this->copy_dir($file, $target_file, $mode, $recursive, $maintain_attributes, $check_difference);
+
+			} elseif(!$check_difference || !file_exists($target_file) || filemtime($file) > filemtime($target_file)){
+				// Nonexistent or allowed to overwrite
+				$this->copy_file($file, $target_file, $maintain_attributes);
 			}
 		}
 	}
