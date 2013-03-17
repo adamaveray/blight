58dbd4a5bebca72be713cc8cd82350bb87673ce9
Adam Averay <adam.averay@gmail.com>
2013-03-17 12:07:21 -0400

    Updated packages builder to build themes

--- prepare_packages.php
+++ prepare_packages.php
@@ -1,26 +1,30 @@
 <?php
-$base_dir	= __DIR__.'/blog-data/plugins/';
-$dirs		= glob($base_dir.'/*');
+$base_dirs	= array(
+	__DIR__.'/blog-data/plugins/',
+	__DIR__.'/blog-data/themes/'
+);
 
-$phar_dirs	= array();
+foreach($base_dirs as $base_dir){
+	$dirs	= glob($base_dir.'/*');
 
-foreach($dirs as $dir){
-	if(!is_dir($dir)){
-		continue;
+	foreach($dirs as $dir){
+		if(!is_dir($dir)){
+			continue;
+		}
+
+		$name	= basename($dir).'.phar';
+
+		try {
+			$phar = new Phar($base_dir.'/'.$name, 0, $name);
+			$phar->buildFromDirectory($dir);
+			$phar->setStub('<?php __HALT_COMPILER();');
+			unset($phar);
+
+		} catch(\Exception $e){
+			echo 'Could not package `'.$name.'` (Exception: '.$e->getMessage().' ['.$e->getFile().'#'.$e->getLine().'])'.PHP_EOL;
+			continue;
+		}
+
+		echo 'Packaged `'.$name.'`'.PHP_EOL;
 	}
-
-	$name	= basename($dir).'.phar';
-
-	try {
-		$phar = new Phar($base_dir.'/'.$name, 0, $name);
-		$phar->buildFromDirectory($dir);
-		$phar->setStub('<?php __HALT_COMPILER();');
-		unset($phar);
-
-	} catch(\Exception $e){
-		echo 'Could not package `'.$name.'` (Exception: '.$e->getMessage().' ['.$e->getFile().'#'.$e->getLine().'])'.PHP_EOL;
-		continue;
-	}
-
-	echo 'Packaged `'.$name.'`'.PHP_EOL;
 }
