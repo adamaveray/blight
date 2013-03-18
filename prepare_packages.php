cedd36a51e9f47d396dafc7e467a939588f6328a
Adam Averay <adam.averay@gmail.com>
2013-03-17 23:36:00 -0400

    Updated package preparer to also build default theme

--- prepare_packages.php
+++ prepare_packages.php
@@ -1,4 +1,28 @@
 <?php
+function build_package($source, $target_dir){
+	$name	= basename($source).'.phar';
+
+	try {
+		$phar = new Phar($target_dir.'/'.$name, 0, $name);
+		$phar->buildFromDirectory($source);
+		$phar->setStub('<?php __HALT_COMPILER();');
+		unset($phar);
+
+	} catch(\Exception $e){
+		echo 'Could not package `'.$name.'` (Exception: '.$e->getMessage().' ['.$e->getFile().'#'.$e->getLine().'])'.PHP_EOL;
+		return;
+	}
+
+	echo 'Packaged `'.$name.'`'.PHP_EOL;
+}
+
+// Build default theme
+$default_theme	= __DIR__.'/default-theme/Basic/';
+if(is_dir($default_theme)){
+	build_package($default_theme, __DIR__.'/blog-data/themes/');
+}
+
+// Build additional packages
 $base_dirs	= array(
 	__DIR__.'/blog-data/plugins/',
 	__DIR__.'/blog-data/themes/'
@@ -12,19 +36,6 @@ foreach($base_dirs as $base_dir){
 			continue;
 		}
 
-		$name	= basename($dir).'.phar';
-
-		try {
-			$phar = new Phar($base_dir.'/'.$name, 0, $name);
-			$phar->buildFromDirectory($dir);
-			$phar->setStub('<?php __HALT_COMPILER();');
-			unset($phar);
-
-		} catch(\Exception $e){
-			echo 'Could not package `'.$name.'` (Exception: '.$e->getMessage().' ['.$e->getFile().'#'.$e->getLine().'])'.PHP_EOL;
-			continue;
-		}
-
-		echo 'Packaged `'.$name.'`'.PHP_EOL;
+		build_package($dir, $base_dir);
 	}
 }
