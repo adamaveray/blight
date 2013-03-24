30781e1745e6f8f317c9606dc442c3e6a9c87f5d
Adam Averay <adam.averay@gmail.com>
2013-03-23 23:43:30 -0400

    Update builder to modify Composer class map to work with Phar

--- build.php
+++ build.php
@@ -1,7 +1,8 @@
 <?php
+$dir	= 'blight';
 $name	= 'Blight.phar';
 
-$path_source	= __DIR__.'/blight';
+$path_source	= __DIR__.'/'.$dir;
 $path_build		= __DIR__;
 
 try {
@@ -11,6 +12,15 @@ try {
 
 	$phar = new Phar($path_build.'/'.$name, 0, $name);
 	$phar->buildFromDirectory($path_source);
+
+	// Update Composer to handle Phar
+	$path	= 'vendor/composer/autoload_classmap.php';
+	if(file_exists(__DIR__.'/'.$dir.'/'.$path)){
+		$content	= file_get_contents(__DIR__.'/'.$dir.'/'.$path);
+		$content	= str_replace('/'.$dir.'/', '/'.$name.'/', $content);
+		$phar->addFromString($path, $content);
+	}
+
 	$phar->setStub(file_get_contents($path_source.'/phar-stub.php'));
 	unset($phar);
 
